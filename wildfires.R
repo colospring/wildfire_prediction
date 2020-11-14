a4393122f190ca43bda9bb290954f36282ce607f
gklevans <gklevans7@gmail.com>
2020-11-14 08:13:24 -0800

    import data for wildfire analysis

--- wildfires.R
+++ wildfires.R
@@ -2,10 +2,25 @@
 library(RSQLite)
 con <- dbConnect(drv=RSQLite::SQLite(), 
                  dbname="/Users/gregoryklevans/Downloads/FPA_FOD_20170508.sqlite")
-fires <- dbReadTable(con, 'Fires')
+fires <- dbReadTable(con, "Fires")
 
 #Get calendar dates
-years <- sapply(fires$FIRE_YEAR, toString)
-jan1 <- replicate(length(fires$FIRE_YEAR), "-01-01")
-fires$DATE <- paste0(years, jan1)
-fires$DATE <- as.Date(fires$DISCOVERY_DOY, origin = fires$DATE)
+fires$DATE <- sapply(fires$FIRE_YEAR, toString)    #convert fire years to string
+fires$DATE <- paste0(fires$DATE , "-01-01")      #set dates to beginning of year
+fires$DATE <- as.Date(fires$DISCOVERY_DOY, origin=fires$DATE)   #add day of year
+
+#Import fire weather data
+library(haven)
+fire_weather <- read_dta("/Volumes/NO NAME/WFAS/fdr.dta")
+#Get calendar dates: Stata date origin is Jan 1, 1960
+fire_weather$Date <- as.Date(fire_weather$Date, origin="1960-01-01")
+
+#Convert data to spatial points data frames
+library(sp)
+coordinates(fires) <- ~ LONGITUDE + LATITUDE
+coordinates(fire_weather) <- ~ Long + Lat
+
+#Import fire weather zone shapefiles
+library(rgdal)
+fwz <- readOGR(dsn= "/Users/gregoryklevans/Downloads/fz03mr20", 
+               layer="fz03mr20", verbose=FALSE)
